1f6d956661d2ba567ceb4734832e95ecb7870b72
timakin <timaki.st@gmail.com>
2016-05-05 04:47:15 +0900

    [add] all methods

--- server.go
+++ server.go
@@ -8,8 +8,28 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+	e.POST("/api/notifications/enqueue", func(c echo.Context) error {
+		return c.String(http.StatusOK, "POST enqueue")
 	})
+	e.PUT("/api/notifications/publish", func(c echo.Context) error {
+		return c.String(http.StatusOK, "PUT publish")
+	})
+	e.GET("/api/notifications/subscriptions", func(c echo.Context) error {
+		return c.String(http.StatusOK, "GET subscriptions")
+	})
+	e.PUT("/api/notifications/subscribe", func(c echo.Context) error {
+		return c.String(http.StatusOK, "PUT subscribe")
+	})
+
+	e.POST("/api/messages", func(c echo.Context) error {
+		return c.String(http.StatusOK, "POST messages")
+	})
+	e.GET("/api/messages", func(c echo.Context) error {
+		return c.String(http.StatusOK, "GET messages")
+	})
+	e.GET("/api/messages/:id", func(c echo.Context) error {
+		return c.String(http.StatusOK, "GET message"+c.Param("id"))
+	})
+
 	e.Run(standard.New(":3000"))
 }
