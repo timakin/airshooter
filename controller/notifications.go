063626b383c3a546f3bfe866fe0e8b4c58ddabc9
timakin <timaki.st@gmail.com>
2016-05-05 18:19:28 +0900

    [fix] shift notification CRUD to controller directory

--- controller/notifications.go
+++ controller/notifications.go
@@ -13,3 +13,15 @@ type Notifications []Notification
 func EnqueueNotification(c echo.Context) error {
 	return c.String(http.StatusOK, "POST enqueue")
 }
+
+func PublishNotification(c echo.Context) error {
+	return c.String(http.StatusOK, "PUT publish")
+}
+
+func GetNotifications(c echo.Context) error {
+	return c.String(http.StatusOK, "GET subscriptions")
+}
+
+func MarkReadNotifications(c echo.Context) error {
+	return c.String(http.StatusOK, "PUT subscribe")
+}
