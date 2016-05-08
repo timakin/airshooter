9753bdeecdbedc418a29f8644931da57bbfe4feb
timakin <timaki.st@gmail.com>
2016-05-08 22:21:30 +0900

    [add] base GET services

---
+++ service/message.go
@@ -0,0 +1,9 @@
+package service
+
+func getSingle(id int64) {
+    // find a notification by id
+}
+
+func getCollection(ids []int64) {
+    // find notifications by ids
+}
---
+++ service/notification.go
@@ -0,0 +1,10 @@
+package service
+
+
+func getSingle(id int64) {
+    // find a notification by id
+}
+
+func getCollection(ids []int64) {
+    // find notifications by ids
+}
