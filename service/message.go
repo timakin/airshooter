146ae1285c33a520d3bb917283bdcddfc4b59aa6
timakin <timaki.st@gmail.com>
2016-05-11 08:05:23 +0900

    [modify] camelize service

--- service/message.go
+++ service/message.go
@@ -1,9 +1,9 @@
 package service
 
-func getMessage(id int64) {
-    // find a notification by id
+func GetMessage(id int64) {
+	// find a notification by id
 }
 
-func getMessages(ids []int64) {
-    // find notifications by ids
+func GetMessages(ids []int64) {
+	// find notifications by ids
 }
--- service/notification.go
+++ service/notification.go
@@ -1,9 +1,9 @@
 package service
 
-func getNotification(id int64) {
-    // find a notification by id
+func GetNotification(id int64) {
+	// find a notification by id
 }
 
-func getNotifications(ids []int64) {
-    // find notifications by ids
+func GetNotifications(ids []int64) {
+	// find notifications by ids
 }
