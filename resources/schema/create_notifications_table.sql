53c515b3a79dcd214fbd5a44f0c8a564c0a90657
timakin <timaki.st@gmail.com>
2016-07-02 01:10:40 +0900

    [fix] notifications sql

--- resources/schema/create_notifications_table.sql
+++ resources/schema/create_notifications_table.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS notifications (
     `id`           int(10) unsigned not null auto_increment,
     `title`        varchar(255)  not null default '',
+    `status`        varchar(32)  not null default '',
     `text`         text not null,
+    `delivers_at`   int(10) unsigned,
     `expires_at`   int(10) unsigned,
     `created_at`   int(10) unsigned not null,
     `updated_at`   int(10) unsigned not null,
