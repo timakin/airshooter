cef66d5fdd3af1f7de57d978c45626734f71913a
timakin <timaki.st@gmail.com>
2016-07-04 06:03:28 +0900

    [add] message service

--- service/message.go
+++ service/message.go
@@ -1,13 +1,45 @@
 package service
 
-// import (
-// 	m "github.com/timakin/airshooter/model"
-// )
-//
-// func GetMessage(id int64) (*m.Message, error) {
-// 	return nil, nil
-// }
-//
-// func GetMessages(ids []int64) ([]*m.Message, error) {
-// 	return nil, nil
-// }
+import (
+	constant "github.com/timakin/airshooter/constant"
+	db "github.com/timakin/airshooter/datasource"
+	m "github.com/timakin/airshooter/model"
+	"time"
+)
+
+func EnqueueMessage(message *m.Message) (result *m.Message, err error) {
+	createdAt := time.Now().Unix()
+	updatedAt := time.Now().Unix()
+
+	sender := m.MessageSender{
+		SenderId:   message.MessageSender.SenderId,
+		SenderType: message.MessageSender.SenderType,
+		CreatedAt:  &createdAt,
+		UpdatedAt:  &updatedAt,
+	}
+	recipient := m.MessageRecipient{
+		RecipientId:   message.MessageRecipient.RecipientId,
+		RecipientType: message.MessageRecipient.RecipientType,
+		CreatedAt:     &createdAt,
+		UpdatedAt:     &updatedAt,
+	}
+
+	message.CreatedAt = &createdAt
+	message.UpdatedAt = &updatedAt
+	message.MessageSender = sender
+	message.MessageRecipient = recipient
+
+	if result, err = db.InsertMessage(message); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func GetMessages(threadId *int64) (results *[]m.Message, err error) {
+	if results, err = db.SelectMessages(threadId); err != nil {
+		return nil, err
+	}
+
+	return results, nil
+}
