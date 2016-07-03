e830c93ca21a56b2fb3375335c4130bb18ff989c
timakin <timaki.st@gmail.com>
2016-07-04 05:49:33 +0900

    [add] messages route

--- controller/messages.go
+++ controller/messages.go
@@ -1,18 +1,15 @@
 package controller
 
-// import (
-// 	"github.com/labstack/echo"
-// 	m "github.com/timakin/airshooter/model"
-// 	"net/http"
-// )
-//
-// func PostMessage(c echo.Context) error {
-// 	return c.String(http.StatusOK, "POST messages")
-// }
-//
-// func GetMessages(c echo.Context) error {
-// 	message := new(m.Message)
-// 	test := "test"
-// 	message.Text = &test
-// 	return c.JSON(http.StatusOK, message)
-// }
+import (
+	"github.com/labstack/echo"
+	m "github.com/timakin/airshooter/model"
+	"net/http"
+)
+
+func SendMessage(c echo.Context) error {
+	return c.String(http.StatusOK, "POST messages")
+}
+
+func ReceiveMessages(c echo.Context) error {
+	return c.JSON(http.StatusOK, message)
+}
