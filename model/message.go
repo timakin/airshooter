c7d1f9bcc00ff8190e37423896fcc7cecc0b5d11
timakin <timaki.st@gmail.com>
2016-07-09 10:08:24 +0900

    [fix] model of message

--- model/message.go
+++ model/message.go
@@ -2,7 +2,6 @@ package model
 
 type Message struct {
 	Text             *string          `json:"text" validate:"required"`
-	ThreadId         *int64           `json:"thread_id" validate:"required"`
 	MessageSender    MessageSender    `json:"sender"`
 	MessageRecipient MessageRecipient `json:"recipient"`
 	CreatedAt        *int64           `json:"created_at"`
