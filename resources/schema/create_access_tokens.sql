fd045699067dbf1724cf5d94c2ea03cb2f7d69c0
timakin <timaki.st@gmail.com>
2016-07-20 08:19:03 +0900

    [fix] varchar(1024)

--- resources/schema/create_access_tokens.sql
+++ resources/schema/create_access_tokens.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS access_tokens (
-    `id`         int(10) unsigned not null auto_increment,
-    `token`      varchar(256) not null default "",
+    `id`			  int(10) unsigned not null auto_increment,
+    `token`			  varchar(1024) not null default "",
     `created_at`      int(10) unsigned not null,
     `updated_at`      int(10) unsigned not null,
     PRIMARY KEY (`id`)
