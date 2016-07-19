01ed5ed17c7f7ab045dfddccc16573b041977e31
timakin <timaki.st@gmail.com>
2016-07-20 01:47:05 +0900

    [modify] clients table

--- resources/schema/create_clients.sql
+++ resources/schema/create_clients.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS clients (
-    `uid`         varchar(32) not null default "",
-    `secret`      varchar(32) not null default "",
+    `uid`         varchar(128) not null default "",
+    `secret`      varchar(128) not null default "",
     `created_at`  int(10) unsigned not null,
     `updated_at`  int(10) unsigned not null,
     PRIMARY KEY (`uid`)
