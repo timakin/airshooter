747e26be2a819c0700e08681b4d4a270df7ad8e6
timakin <timaki.st@gmail.com>
2016-08-01 09:31:54 +0900

    [add] client test

--- service/client_test.go
+++ service/client_test.go
@@ -6,3 +6,18 @@ import (
 
 	. "gopkg.in/check.v1"
 )
+
+func Test(t *testing.T) { TestingT(t) }
+
+type TestSuite struct{}
+
+var _ = Suite(&TestSuite{})
+
+func (s *MySuite) SetUpTest(c *C) {
+	s.dir = c.MkDir()
+	// Use s.dir to prepare some data.
+}
+
+func (s *TestSuite) TestGetClient(c *C) {
+	c.Assert(1, Equals, m.Client{})
+}
