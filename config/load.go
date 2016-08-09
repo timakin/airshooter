8eb510401beb9714de75d53f29e127e731d86e98
timakin <timaki.st@gmail.com>
2016-08-09 23:51:23 +0900

    [add] configor

--- config/load.go
+++ config/load.go
@@ -5,17 +5,18 @@ import (
 	"path/filepath"
 )
 
-func Load() interface{} {
-	var Config = struct {
-		DB struct {
-			Name     string
-			User     string `default:"root"`
-			Password string `required:"true" env:"DBPassword"`
-			Port     uint   `default:"3306"`
-		}
-	}{}
+type Config struct {
+	DB struct {
+		Name     string
+		User     string `default:"root"`
+		Password string `required:"true" env:"DBPassword"`
+		Port     uint   `default:"3306"`
+	}
+}
 
+func Load() *Config {
+	loaded := &Config{}
 	absConfigPath, _ := filepath.Abs("./config.yml")
-	configor.Load(&Config, absConfigPath)
-	return &Config
+	configor.Load(loaded, absConfigPath)
+	return loaded
 }
--- datasource/base.go
+++ datasource/base.go
@@ -4,7 +4,10 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/pkg/errors"
+	"github.com/timakin/airshooter/config"
 	"github.com/timakin/airshooter/constant"
+
+	"strconv"
 )
 
 var sharedInstance *gorm.DB
@@ -12,7 +15,9 @@ var sharedInstance *gorm.DB
 // Singleton
 func GetDBInstance() (*gorm.DB, error) {
 	if sharedInstance == nil {
-		db, err := gorm.Open("mysql", "root:@/airshooter?charset=utf8&parseTime=True&loc=Local")
+		setting := config.Load()
+		address := setting.DB.User + ":" + setting.DB.Password + "@" + strconv.Itoa(int(setting.DB.Port)) + "/" + setting.DB.Name + "?charset=utf8&parseTime=True&loc=Local"
+		db, err := gorm.Open("mysql", address)
 		if err != nil {
 			return nil, errors.Wrap(err, constant.ErrDBConnectionFailed)
 		}
